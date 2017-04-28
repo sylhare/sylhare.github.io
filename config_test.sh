8dddf1694ba47adc3ec5a6779ff6908445847d26
sylhare <sylhareng@gmail.com>
2017-04-28 09:04:44 -0400

    Trying some configs

--- config_test.sh
+++ config_test.sh
@@ -2,18 +2,19 @@
 # Compiling the java files and running them
 
 #Start xvfb
-Xvfb :1 -screen 0 800x600x16
+Xvfb :1 -screen 0 800x600x16 +extension RANDR
+test=$?
 xdpyinfo -display :1 >/dev/null 2>&1 && echo "In use" || echo "Free"
 #Check if display 99 of xfvb is used
 xdpyinfo -display :99 >/dev/null 2>&1 && echo "In use" || echo "Free"
 
 export DISPLAY=:1.0
 
-timeout 100 firefox "https://www.youtube.com/watch?v=wdxlc2UdAmg"
+timeout 99 firefox "https://www.youtube.com/watch?v=wdxlc2UdAmg"
 movie=$? 
 
-echo "Movie Status: $movie"
+echo "Movie Status: $movie with view $test"
 
-sleep 90 #To put the system on hold for 90 seconds
+sleep 10 #To put the system on hold for 90 seconds
 
 exit 0
