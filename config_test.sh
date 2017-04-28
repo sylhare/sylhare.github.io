8e3f78257c76ea7234a12ea615f894fa6b70b3c3
sylhare <sylhareng@gmail.com>
2017-04-28 09:57:56 -0400

    Configs

--- config_test.sh
+++ config_test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # Compiling the java files and running them
 
-#Start xvfb
+
 export DISPLAY=:99.0
 #export DISPLAY=:1.0
+
+#Start xvfb
 echo "starting screen"
-Xvfb :99 -screen 0 800x600x16 +extension RANDR & || echo "fail"
-test=$?
-xdpyinfo -display :1 >/dev/null 2>&1 && echo "In use" || echo "Free"
+#Xvfb :99 -screen 0 800x600x16 +extension RANDR || echo "fail"
+#test=$?
+#xdpyinfo -display :1 >/dev/null 2>&1 && echo "In use" || echo "Free"
 #Check if display 99 of xfvb is used (should be the one in use)
 xdpyinfo -display :99 >/dev/null 2>&1 && echo "In use" || echo "Free"
 
@@ -15,7 +17,7 @@ echo "starting movie"
 timeout 99 firefox "https://www.youtube.com/watch?v=wdxlc2UdAmg"
 movie=$? 
 
-echo "Movie Status: $movie with view $test"
+echo "Movie Status: $movie"
 
 sleep 10 #To put the system on hold for 90 seconds
 
