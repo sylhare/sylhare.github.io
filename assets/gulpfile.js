8bf3a151fd89562f20d34b4e03d0e35b238c8a35
sylhare <sylhare@outlook.com>
2020-09-15 11:36:29 -0400

    Add other image minifier

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -5,6 +5,7 @@
  */
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
+const responsive = require('gulp-responsive'); // Supported formats: heic, heif, jpeg, jpg, png, raw, tiff, webp
 
 gulp.task("img", function imging() {
   return gulp.src('img/**/*.{png,svg,jpg,webp,jpeg,gif}')
@@ -14,5 +15,36 @@ gulp.task("img", function imging() {
     })
     .pipe(gulp.dest('img/'))
 });
+
+// Alternative using "sharp" in case "imagemin" does not work
+gulp.task('sharp_img', function () {
+  let settings = {
+    quality: 60,
+    progressive: true,
+    compressionLevel: 9,
+  };
+
+  return gulp.src('img/**/*.{png,jpg,webp,jpeg}')
+    .pipe(responsive({
+      '**/*.*': settings,
+      '*.*': settings,
+    }))
+    .pipe(gulp.dest('img'))
+});
+
+gulp.task('thumbnails', function () {
+  let settings = {
+    width: '50%',
+    //format: 'jpeg', // convert to jpeg format
+  };
+
+  return gulp.src('img/**/*.{png,jpg,webp,jpeg}')
+    .pipe(responsive({
+      '**/*.*': settings,
+      '*.*': settings,
+    }))
+    .pipe(gulp.dest('thumbnails'))
+});
+
 
 gulp.task("default", gulp.series(gulp.parallel('img')));
