d3adb5b9427f4012425353ffed486b78013ad9e5
sylhare <sylhare@outlook.com>
2023-03-16 14:17:14 -0400

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,5 @@
-/*
- * Here are all of the gulp tasks you can use to help manage your blog
- * Use `npm install` to install all the dependencies located in package.json
- * Then `gulp default` to minimize css and images.
- */
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
-const responsive = require('gulp-responsive'); // Supported formats: heic, heif, jpeg, jpg, png, raw, tiff, webp
 const fs = require('fs');
 
 const assetsPath = './assets/'
@@ -44,38 +38,6 @@ gulp.task("img", function imging() {
       console.log(err.toString());
     })
     .pipe(gulp.dest(paths.img.dest))
-});
-
-//Alternative using "sharp" in case "imagemin" does not work
-gulp.task('sharp_img', function () {
-  let settings = {
-    quality: 80,
-    progressive: true,
-    compressionLevel: 9,
-  };
-
-  return gulp.src(paths.img.sharp)
-    .pipe(responsive({
-      '**/*.*': settings,
-      '*.*': settings,
-    }))
-    .pipe(gulp.dest(paths.img.dest))
-});
-
-
-gulp.task('thumbnails', function () {
-  let settings = {
-    width: '50%',
-    //format: 'jpeg', // convert to jpeg format
-  };
-
-  return gulp.src(paths.img.src)
-    .pipe(responsive({
-      '**/*.*': settings,
-      '*.*': settings,
-      //  'assets/img/specific.jpg': { width: '12.5%'}
-    }))
-    .pipe(gulp.dest(paths.img.thumbnails))
 });
 
 
